8b3747b124d555f978d1cd62a59797b3d8eb7505
Crystal Ren <crystalren97@gmail.com>
2024-12-14 22:47:49 -0500

    add tcp connector fields

--- config/database.php
+++ config/database.php
@@ -59,7 +59,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'), #'/certs/ca.pem',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false), # todo try true?
+                PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY'), #'/certs/client-key.pem',
+                PDO::MYSQL_ATTR_SSL_CERT => env('MYSQL_ATTR_SSL_CERT'), #'/certs/client-cert.pem',
             ]) : [],
         ],
 

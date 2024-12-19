b077a9e262fc41ff78db62e598ce15192bcc98b3
Crystal Ren <crystalren97@gmail.com>
2024-12-19 00:41:22 -0500

    private ip cloudbuild migration not working

--- db-migrate.sh
+++ db-migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo
+#sudo
 
 apt-get update && apt-get install -y \
     zip \
@@ -15,18 +15,18 @@ apt-get clean && rm -rf /var/lib/apt/lists/*
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 echo "xxxxxxxxxxxxxx$USER"
-chown -R $USER:$USER ./
-chgrp -R $USER storage bootstrap/cache && chmod -R ug+rwx storage bootstrap/cache
+#chown -R $USER:$USER ./
+#chgrp -R $USER storage bootstrap/cache && chmod -R ug+rwx storage bootstrap/cache
 
-composer install
+#composer install
 
-php artisan optimize:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+#php artisan optimize:clear
+#php artisan config:cache
+#php artisan route:cache
+#php artisan view:cache
 
 # Run Laravel migration (by force, since it would be a prod-environment)
-php artisan migrate --force
+#php artisan migrate --force
 
 
 # curl -sL https://deb.nodesource.com/setup_${NODE_VERSION}.x | bash - &&  apt-get install -y nodejs

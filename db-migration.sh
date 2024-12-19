272add1d71b5362f9e3a997b86d076c9b70690f9
Crystal Ren <crystalren@google.com>
2024-12-18 19:18:41 -0500

    debugging CI/CD on cloud run

--- db-migration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run Laravel migration (by force, since it would be a prod-environment)
-php artisan migrate --force
-
-# Run Apache in "foreground" mode (the default mode that runs in Docker)
-apache2-foreground

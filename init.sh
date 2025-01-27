14f4cb0a5e55a3dab3ddc64fd52b350e0b5f9444
Crystal Ren <crystalren@google.com>
2025-01-27 07:44:10 -0500

    try chmod mted vol

--- init.sh
+++ init.sh
@@ -2,15 +2,6 @@
 
 # This workaround required bc postgres enforces file permissions on the cert files.
 
-mkdir "/etc/ssl/private/pg_certs/"
-cp -v "$SSL_CA_PATH" "/etc/ssl/private/pg_certs/server-ca.pem"
-cp -v "$SSL_KEY_PATH" "/etc/ssl/private/pg_certs/client-key.pem"
-cp -v "$SSL_CERT_PATH" "/etc/ssl/private/pg_certs/client-cert.pem"
-
-chmod -R 600 "/etc/ssl/private/pg_certs"
-
-export SSL_CA_PATH="/etc/ssl/private/pg_certs/server-ca.pem"
-export SSL_KEY_PATH="/etc/ssl/private/pg_certs/client-key.pem"
-export SSL_CERT_PATH="/etc/ssl/private/pg_certs/client-cert.pem"
+chmod -R 600 "/var/www/html/certs"
 
 exec "$@"

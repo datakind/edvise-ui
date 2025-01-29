d7f10771361bfc68b22fe988fb295982021d5f79
Crystal Ren <crystalren@google.com>
2025-01-29 09:49:59 -0500

    change colors

--- resources/js/Components/Steppers.jsx
+++ resources/js/Components/Steppers.jsx
@@ -8,20 +8,20 @@ const Steppers = ({ currentStep }) => {
     ];
 
     return (
-        <div className="flex justify-center mb-8">
+        <div className="flex justify-center pt-24 pb-24">
             {steps.map((step, index) => (
                 <div key={step.step} className="flex flex-col items-center mx-12 relative">
                     <div
-                        className={`rounded-full w-10 h-10 flex items-center justify-center border-2 
-                            ${currentStep === step.step ? 'border-[#f79222] text-black font-bold' : 'border-gray-400 text-gray-600'}`}
+                        className={`rounded-full w-8 h-8 flex items-center justify-center border-2 
+                            ${currentStep === step.step ? 'border-[#f79222] text-[#f79222] font-bold' : 'border-gray-300 bg-gray-300 text-black'}`}
                     >
                         {step.step}
                     </div>
-                    <span className={`text-base font-medium text-center mt-2 ${currentStep >= step.step ? 'text-black' : 'text-gray-600'}`}>
+                    <span className="text-base font-medium text-semibold text-center mt-2 text-black">
                         {step.label}
                     </span>
                     {index < steps.length - 1 && (
-                        <div className={`h-[2px] w-20 absolute top-1/2 -translate-y-1/2 left-[calc(50%+5.5rem)] -translate-x-1/2 bg-gray-400`}></div>
+                        <div className="h-[2px] w-20 absolute top-1/2 -translate-y-1/2 left-[calc(50%+6rem)] -translate-x-1/2 bg-gray-300"></div>
                     )}
                 </div>
             ))}

2ecdb479ef4f384b7ea969182921384447769c21
Crystal Ren <crystalren97@gmail.com>
2025-02-09 14:30:12 -0500

    pass through access types in create inst form

--- resources/js/Pages/CreateInst.jsx
+++ resources/js/Pages/CreateInst.jsx
@@ -48,7 +48,8 @@ return (
                       name={id+"-access"}
                       className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     >
-                      <option>Institution Researcher</option>
+                      <option>MODEL_OWNER</option>
+                      <option>VIEWER</option>
                     </select>
                   </div>
                 </div>

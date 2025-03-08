6e7852b2e584c1a409eb6148b267ffa2f0afd28d
Crystal Ren <crystalren97@gmail.com>
2025-03-07 21:17:19 -0500

    make model history table flexible

--- resources/js/Components/ModelRunHistory.jsx
+++ resources/js/Components/ModelRunHistory.jsx
@@ -7,6 +7,8 @@ import classNames from 'classnames';
 
 const ModelRunHistory = (props) => {
     const [dataToDisplay, setDataToDisplay] = useState([]);
+    const [err, setErr] = useState(null);
+
     useEffect(() => {
         let runvars = props.runInfos;
         try {
@@ -23,92 +25,40 @@ const ModelRunHistory = (props) => {
                 setDataToDisplay([]);
             }
         } catch (err) {
-            console.log(JSON.stringify(err));
-        /*if (err.response != null && err.response.data != null && err.response.data.error != null) {          
-          setError(Error(err.response.data.error));
-        } else {
-          setError(err);
-        }*/
-        } finally {
-        //setLoading(false);
+            setErr(err);
         }
-
     }, [props]);
 
     return (
-        <div className="mt-8 pb-12">
-            <h3 className="text-lg font-semibold mb-2 flex flex-row gap-x-3"><ArrowUturnLeftIcon aria-hidden="true" className="size-6 shrink-0"/> Model Run History</h3> {/* Heading remains */}
-            <div className="flex overflow-hidden flex-wrap items-start self-stretch w-full text-sm font-medium leading-none text-gray-700 bg-white rounded-lg shadow max-md:mt-10">
-                <div className="flex flex-col grow shrink min-w-[240px] w-[257px]">
-                    <TableHeader title="DATE" />
-                    {dataToDisplay.map((run, index) => (
-                        <React.Fragment key={index}>
-                            <TableCell>{run.date}</TableCell>
-                            {index !== dataToDisplay.length - 1 && <TableDivider />}
-                        </React.Fragment>
-                    ))}
-                </div>
-                <div className="flex flex-col grow shrink text-base leading-none text-gray-700 min-w-[240px] w-[222px]">
-                    <TableHeader title="USER" />
-                    {dataToDisplay.map((run, index) => (
-                        <React.Fragment key={index}>
-                            <TableCell>
-                                <div className="flex gap-4 items-center">
-                                    <div className="self-stretch my-auto">{run.user}</div>
-                                </div>
-                            </TableCell>
-                            {index !== dataToDisplay.length - 1 && <TableDivider />}
-                        </React.Fragment>
-                    ))}
-                </div>
-                <div className="flex flex-col grow shrink whitespace-nowrap w-[183px]">
-                    <TableHeader title="BATCH" />
-                    {dataToDisplay.map((run, index) => (
-                        <React.Fragment key={index}>
-                            <TableCell>{run.batch}</TableCell>
-                            {index !== dataToDisplay.length - 1 && <TableDivider />}
-                        </React.Fragment>
-                    ))}
-                </div>
-                <div className="flex flex-col grow shrink min-w-[240px] w-[211px]">
-                    <TableHeader title="OUTPUT FILE" />
-                    {dataToDisplay.map((run, index) => (
-                        <React.Fragment key={index}>
-                            <TableCell>
-                                <div className='pr-0 w-[26px]'>
-                                {run.outputFile == "Pending" ? (<>Pending</>) :
-                                 (<a href={run.outputLink}>Download</a>)}</div>
-                            </TableCell>
-                            {index !== dataToDisplay.length - 1 && <TableDivider />}
-                        </React.Fragment>
-                    ))}
-                </div>
-            </div>
-        </div>
+
+<div className='w-full flex pt-16 pb-12'>
+    <h3 className="text-lg font-semibold mb-2 flex flex-row gap-x-3">
+    <ArrowUturnLeftIcon aria-hidden="true" className="size-6 shrink-0"/> Model Run History</h3>
+  <table className='min-w-[60%] max-w-[90%] table-auto text-left rounded-lg bg-white shadow-md' id="model-history-table">
+    <thead>
+      <tr className='bg-gray-50 border-b border-gray-300 text-gray-500 text-xs font-medium leading-normal tracking-[0.6px] uppercase'>
+        <th className='p-4 px-6'>DATE</th>
+        <th className='p-4 px-6'>USER</th>
+        <th className='p-4 px-6'>BATCH</th>
+        <th className='p-4 px-6'>OUTPUT FILE</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {err != null ? (<tr className='border-b border-gray-300 text-gray-700 text-sm font-normal leading-5' key="error">{err.response.data}</tr>) : 
+      dataToDisplay.map((run, index) => (
+        <tr className='border-b border-gray-300 text-gray-700 text-sm font-normal leading-5' key={run.date}>
+          <td className='p-4 px-6'>{run.date}</td>
+          <td className='p-4 px-6'>{run.user}</td>
+          <td className='p-4 px-6 font-medium'>{run.batch}</td>
+          <td className='p-4 px-6'>{run.outputFile == "Pending" ? (<>Pending</>) : (<a href={run.outputLink}>Download</a>)}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
     );
 };
-
-function TableHeader({ title }) {
-    return (
-        <div className="flex flex-col w-full text-xs tracking-wide text-gray-500 uppercase whitespace-nowrap">
-            <div className="gap-2.5 self-stretch px-6 py-3 bg-gray-50 max-md:px-5">
-                {title}
-            </div>
-            <div className="z-10 shrink-0 h-px bg-gray-200 border border border-solid" />
-        </div>
-    );
-}
-
-function TableCell({ children }) {
-    return (
-        <div className="flex flex-col justify-center items-start px-6 py-7 max-w-full min-h-[72px] w-[325px] max-md:px-5">
-            {children}
-        </div>
-    );
-}
-
-function TableDivider() {
-    return <div className="w-full bg-gray-200 border border border-solid min-h-[1px]" />;
-}
 
 export default ModelRunHistory;

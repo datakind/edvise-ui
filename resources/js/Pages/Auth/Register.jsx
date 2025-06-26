2a8232f77166926b6b00064daae8fae1229856b7
Larry Kilroy <larry@datakind.org>
2025-06-26 11:48:20 +0200

    Hide social login and registration on production

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -39,168 +39,188 @@ export default function Register() {
             alt="Edvise Logo"
           />
 
-          <div className="pb-8 text-2xl">
-            Welcome! Create an account to get started:
-          </div>
-
-          <form onSubmit={onSubmit}>
-            <div>
-              <InputLabel htmlFor="name">Name</InputLabel>
-              <TextInput
-                id="name"
-                type="text"
-                className="mt-1 block w-full"
-                value={form.data.name}
-                onChange={e => form.setData('name', e.currentTarget.value)}
-                required
-                autoFocus
-                autoComplete="name"
-              />
-              <InputError className="mt-2" message={form.errors.name} />
-            </div>
-
-            <div className="mt-4">
-              <InputLabel htmlFor="email">Email</InputLabel>
-              <TextInput
-                id="email"
-                type="email"
-                className="mt-1 block w-full"
-                value={form.data.email}
-                onChange={e => form.setData('email', e.currentTarget.value)}
-                required
-              />
-              <InputError className="mt-2" message={form.errors.email} />
-            </div>
-
-            <div className="mt-4">
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <TextInput
-                id="password"
-                type="password"
-                className="mt-1 block w-full"
-                value={form.data.password}
-                onChange={e => form.setData('password', e.currentTarget.value)}
-                required
-                autoComplete="new-password"
-              />
-              <InputError className="mt-2" message={form.errors.password} />
-            </div>
-
-            <div className="mt-4">
-              <InputLabel htmlFor="password_confirmation">
-                Confirm Password
-              </InputLabel>
-              <TextInput
-                id="password_confirmation"
-                type="password"
-                className="mt-1 block w-full"
-                value={form.data.password_confirmation}
-                onChange={e =>
-                  form.setData('password_confirmation', e.currentTarget.value)
-                }
-                required
-                autoComplete="new-password"
-              />
-              <InputError
-                className="mt-2"
-                message={form.errors.password_confirmation}
-              />
-            </div>
-
-            {page.props.jetstream.hasTermsAndPrivacyPolicyFeature && (
-              <div className="mt-8">
-                <InputLabel htmlFor="terms">
-                  <div className="flex items-center">
-                    <Checkbox
-                      name="terms"
-                      id="terms"
-                      checked={form.data.terms}
-                      onChange={e =>
-                        form.setData('terms', e.currentTarget.checked)
-                      }
-                      required
-                    />
-
-                    <div className="ml-2">
-                      I agree to the&nbsp;
-                      <a
-                        target="_blank"
-                        href="/terms-of-service"
-                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      >
-                        Terms of Service
-                      </a>
-                      &nbsp;and&nbsp;
-                      <a
-                        target="_blank"
-                        href="/privacy-policy"
-                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      >
-                        Privacy Policy
-                      </a>
-                    </div>
-                  </div>
-                  <InputError className="mt-2" message={form.errors.terms} />
-                </InputLabel>
+          {window.location.hostname.includes('sst') ? (
+            <>
+              <div className="pb-8 text-2xl">
+                Welcome! Create an account to get started:
               </div>
-            )}
+              <form onSubmit={onSubmit}>
+                <div>
+                  <InputLabel htmlFor="name">Name</InputLabel>
+                  <TextInput
+                    id="name"
+                    type="text"
+                    className="mt-1 block w-full"
+                    value={form.data.name}
+                    onChange={e => form.setData('name', e.currentTarget.value)}
+                    required
+                    autoFocus
+                    autoComplete="name"
+                  />
+                  <InputError className="mt-2" message={form.errors.name} />
+                </div>
 
-            <div className="mt-4 flex items-center justify-end">
-              <Link
-                href={route('login')}
-                className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Already registered?
-              </Link>
+                <div className="mt-4">
+                  <InputLabel htmlFor="email">Email</InputLabel>
+                  <TextInput
+                    id="email"
+                    type="email"
+                    className="mt-1 block w-full"
+                    value={form.data.email}
+                    onChange={e => form.setData('email', e.currentTarget.value)}
+                    required
+                  />
+                  <InputError className="mt-2" message={form.errors.email} />
+                </div>
 
-              <Button
-                type="submit"
-                className={classNames('ml-4', {
-                  'opacity-25': form.processing,
-                })}
-                disabled={form.processing}
-              >
-                Register
-              </Button>
+                <div className="mt-4">
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <TextInput
+                    id="password"
+                    type="password"
+                    className="mt-1 block w-full"
+                    value={form.data.password}
+                    onChange={e =>
+                      form.setData('password', e.currentTarget.value)
+                    }
+                    required
+                    autoComplete="new-password"
+                  />
+                  <InputError className="mt-2" message={form.errors.password} />
+                </div>
+
+                <div className="mt-4">
+                  <InputLabel htmlFor="password_confirmation">
+                    Confirm Password
+                  </InputLabel>
+                  <TextInput
+                    id="password_confirmation"
+                    type="password"
+                    className="mt-1 block w-full"
+                    value={form.data.password_confirmation}
+                    onChange={e =>
+                      form.setData(
+                        'password_confirmation',
+                        e.currentTarget.value,
+                      )
+                    }
+                    required
+                    autoComplete="new-password"
+                  />
+                  <InputError
+                    className="mt-2"
+                    message={form.errors.password_confirmation}
+                  />
+                </div>
+
+                {page.props.jetstream.hasTermsAndPrivacyPolicyFeature && (
+                  <div className="mt-8">
+                    <InputLabel htmlFor="terms">
+                      <div className="flex items-center">
+                        <Checkbox
+                          name="terms"
+                          id="terms"
+                          checked={form.data.terms}
+                          onChange={e =>
+                            form.setData('terms', e.currentTarget.checked)
+                          }
+                          required
+                        />
+
+                        <div className="ml-2">
+                          I agree to the&nbsp;
+                          <a
+                            target="_blank"
+                            href="/terms-of-service"
+                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          >
+                            Terms of Service
+                          </a>
+                          &nbsp;and&nbsp;
+                          <a
+                            target="_blank"
+                            href="/privacy-policy"
+                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          >
+                            Privacy Policy
+                          </a>
+                        </div>
+                      </div>
+                      <InputError
+                        className="mt-2"
+                        message={form.errors.terms}
+                      />
+                    </InputLabel>
+                  </div>
+                )}
+
+                <div className="mt-4 flex items-center justify-end">
+                  <Link
+                    href={route('login')}
+                    className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Already registered?
+                  </Link>
+
+                  <Button
+                    type="submit"
+                    className={classNames('ml-4', {
+                      'opacity-25': form.processing,
+                    })}
+                    disabled={form.processing}
+                  >
+                    Register
+                  </Button>
+                </div>
+              </form>
+              <div className="mt-8 grid grid-cols-5 gap-4">
+                <div className="col-span-2 pt-2">
+                  <hr></hr>
+                </div>
+                <div className="text-center text-sm text-gray-600">or</div>
+                <div className="col-span-2 pt-2">
+                  <hr></hr>
+                </div>
+              </div>
+              <div className="mt-12 flex flex-col space-y-4">
+                <a
+                  href="/auth/google"
+                  className="flex w-full items-center justify-center rounded-full border border-gray-300 bg-white px-4 py-2 shadow-sm hover:bg-gray-100"
+                >
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
+                    alt="Google Logo"
+                    className="mr-2 h-5 w-5"
+                  />
+                  <span className="text-sm font-medium text-gray-700">
+                    Sign up with Google
+                  </span>
+                </a>
+                <a
+                  href="/auth/azure"
+                  className="flex w-full items-center justify-center rounded-full border border-gray-300 bg-white px-4 py-2 shadow-sm hover:bg-gray-100"
+                >
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1200px-Microsoft_logo.svg.png"
+                    alt="Microsoft Logo"
+                    className="mr-2 h-5 w-5"
+                  />
+                  <span className="text-sm font-medium text-gray-700">
+                    Sign up with Microsoft
+                  </span>
+                </a>
+              </div>
+            </>
+          ) : (
+            <div className="pb-8 text-2xl">
+              Edvise is currently not accepting open registrations. Please fill
+              out our "Request Demo" form to{' '}
+              <a href="/" className="font-semibold text-black underline">
+                get started
+              </a>
+              .
             </div>
-          </form>
-          <div className="mt-8 grid grid-cols-5 gap-4">
-            <div className="col-span-2 pt-2">
-              <hr></hr>
-            </div>
-            <div className="text-center text-sm text-gray-600">or</div>
-            <div className="col-span-2 pt-2">
-              <hr></hr>
-            </div>
-          </div>
-          <div className="mt-12 flex flex-col space-y-4">
-            <a
-              href="/auth/google"
-              className="flex w-full items-center justify-center rounded-full border border-gray-300 bg-white px-4 py-2 shadow-sm hover:bg-gray-100"
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
-                alt="Google Logo"
-                className="mr-2 h-5 w-5"
-              />
-              <span className="text-sm font-medium text-gray-700">
-                Sign up with Google
-              </span>
-            </a>
-            <a
-              href="/auth/azure"
-              className="flex w-full items-center justify-center rounded-full border border-gray-300 bg-white px-4 py-2 shadow-sm hover:bg-gray-100"
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1200px-Microsoft_logo.svg.png"
-                alt="Microsoft Logo"
-                className="mr-2 h-5 w-5"
-              />
-              <span className="text-sm font-medium text-gray-700">
-                Sign up with Microsoft
-              </span>
-            </a>
-          </div>
+          )}
           <div className="text-center">
             <AuthFooter />
           </div>

ed908949f331f00b102e0629487a76f24df51879
William Carr <bill@datakind.org>
2026-03-17 14:29:31 -0400

    test: create team test

--- tests/Feature/CreateTeamTest.php
+++ tests/Feature/CreateTeamTest.php
@@ -19,6 +19,6 @@ class CreateTeamTest extends TestCase
         ]);
 
         $this->assertCount(2, $user->fresh()->ownedTeams);
-        $this->assertEquals('Test Team', $user->fresh()->ownedTeams()->latest('id')->first()->name);
+        $this->assertEquals('Test Team', $user->fresh()->ownedTeams()->firstWhere('name', 'Test Team')->name);
     }
 }

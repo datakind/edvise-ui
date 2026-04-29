b20c01c308f7a25f333efae3e1e8630fd2864763
William Carr <bill@datakind.org>
2026-04-29 10:49:52 -0400

    build: generic db migration for dev and qa

--- scripts/run-migrate.sh
+++ scripts/run-migrate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Migrations for a Cloud Run Job (same "web" image as the app). Do not use the default
 # image entrypoint. In the job, set: Command = /bin/sh, Arguments = /app/scripts/run-migrate.sh
-# Give the job the same DB env, Cloud SQL attachment, and network as your dev-frontend.
+# Give the job the same DB env, Cloud SQL attachment, and network as the app for that database.
 set -eu
 cd /app
 exec php artisan migrate --force

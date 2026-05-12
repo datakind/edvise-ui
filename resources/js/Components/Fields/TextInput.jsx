0c806c818098b5552bb458044d878b80873482cb
William Carr <bill@datakind.org>
2026-05-12 07:47:26 -0400

    feat(style): apply Edvise Design System to input elements

--- resources/js/Components/Fields/TextInput.jsx
+++ resources/js/Components/Fields/TextInput.jsx
@@ -4,10 +4,7 @@ const TextInput = forwardRef((props, ref) => (
   <input
     {...props}
     ref={ref}
-    className={classNames(
-      'rounded-full border-gray-300 shadow-sm focus:border-[#F79122] focus:ring-[#F79122]',
-      props.className,
-    )}
+    className={classNames(props.className)}
   />
 ));
 export default TextInput;

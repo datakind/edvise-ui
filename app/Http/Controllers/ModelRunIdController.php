548d4eee631f2754bde712b772a552ef8cb80c4b
William Carr <bill@datakind.org>
2026-05-20 18:14:53 -0400

    chore: remove unused ALT_ model run id lookup

--- app/Http/Controllers/ModelRunIdController.php
+++ app/Http/Controllers/ModelRunIdController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class ModelRunIdController extends Controller
 {
@@ -24,20 +23,5 @@ class ModelRunIdController extends Controller
             'job_id' => $job_run_id,
             'model_id' => $job->model_id,
         ]);
-    }
-
-    /**
-     * Resolve model_run_id from .env by institution (used by DataDictionary).
-     */
-    public function getByInst(string $inst_id, Request $request): JsonResponse
-    {
-        $envKey = $request->query('env_key', 'ALT_'.strtoupper($inst_id));
-        $modelRunId = config('model_run_ids.'.$envKey);
-
-        if (! $modelRunId) {
-            return response()->json(['error' => 'Model run ID not found for institution'], 404);
-        }
-
-        return response()->json(['model_run_id' => $modelRunId]);
     }
 }

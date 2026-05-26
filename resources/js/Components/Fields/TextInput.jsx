e17b9fbdd488f702903bf6b01078dc186ef831a4
William Carr <bill@datakind.org>
2026-05-26 09:33:08 -0400

    fix(eslint): add display name to TextInput forwardRef component

--- resources/js/Components/Fields/TextInput.jsx
+++ resources/js/Components/Fields/TextInput.jsx
@@ -1,10 +1,12 @@
 import classNames from 'classnames';
 import React, { forwardRef } from 'react';
-const TextInput = forwardRef((props, ref) => (
-  <input
-    {...props}
-    ref={ref}
-    className={classNames(props.className)}
-  />
-));
+const TextInput = forwardRef(function TextInput(props, ref) {
+  return (
+    <input
+      {...props}
+      ref={ref}
+      className={classNames(props.className)}
+    />
+  );
+});
 export default TextInput;

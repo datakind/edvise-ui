bfdf3b88fe7e510a63ec3b38233581a660c66f15
William Carr <bill@datakind.org>
2026-07-08 15:17:42 -0400

    chore(tests): remove unused imports from EmailVerificationTest

--- tests/Feature/EmailVerificationTest.php
+++ tests/Feature/EmailVerificationTest.php
@@ -3,10 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 use Laravel\Fortify\Features;
 use Tests\TestCase;
